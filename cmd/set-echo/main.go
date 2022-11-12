f245bf9108c35eb6b85cacab34e1a8528e20b2dc
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2022-11-12 14:19:04 +0000

    broadcasting request to all pods in service

--- cmd/set-echo/main.go
+++ cmd/set-echo/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"fmt"
 	"net/http"
 	"os"
 	"strconv"
 
+	"log"
+
 	"github.com/gin-gonic/gin"
 	redis "github.com/go-redis/redis/v9"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
+
+func ProvideKuberentesClientset() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return kubernetes.NewForConfig(config)
+}
 
 func ProvideRedisClient() *redis.Client {
 	return redis.NewClient(&redis.Options{
@@ -25,10 +42,36 @@ var localState int64
 
 const (
 	stateKey = "state"
+	port     = 8888
 )
+
+func GetServiceEndpoints(
+	ctx context.Context,
+	client *kubernetes.Clientset,
+	serviceName string,
+) ([]string, error) {
+	endpoints := []string{}
+	serviceData, err := client.CoreV1().Endpoints(os.Getenv("POD_NAMESPACE")).Get(
+		ctx,
+		serviceName,
+		metav1.GetOptions{},
+	)
+	if err != nil {
+		return endpoints, err
+	}
+
+	for _, subset := range serviceData.Subsets {
+		for _, address := range subset.Addresses {
+			endpoints = append(endpoints, address.IP)
+		}
+	}
+
+	return endpoints, nil
+}
 
 func ProvideRouter(
 	rc *redis.Client,
+	kc *kubernetes.Clientset,
 ) *gin.Engine {
 	router := gin.Default()
 
@@ -53,6 +96,36 @@ func ProvideRouter(
 				"error": err.Error(),
 			})
 			return
+		}
+
+		endpoints, err := GetServiceEndpoints(ctx, kc, os.Getenv("SERVICE_NAME"))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		for _, endpoint := range endpoints {
+			url := fmt.Sprintf("%s:%d/sync", endpoint, port)
+			req, err := http.NewRequest(
+				http.MethodPost,
+				url,
+				bytes.NewReader([]byte{}),
+			)
+			if err != nil {
+				log.Printf("failed to make request for url %s", url)
+				continue
+			}
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Printf("failed to send request to url %s", url)
+				continue
+			}
+			if resp.StatusCode != http.StatusOK {
+				log.Printf("did not receive 200 code from url %s", url)
+			} else {
+				log.Printf("successfully synced endpoint with IP %s", endpoint)
+			}
 		}
 
 		c.JSON(http.StatusOK, gin.H{})
@@ -93,7 +166,11 @@ func ProvideRouter(
 
 func main() {
 	rc := ProvideRedisClient()
-	router := ProvideRouter(rc)
+	kc, err := ProvideKuberentesClientset()
+	if err != nil {
+		panic(err)
+	}
+	router := ProvideRouter(rc, kc)
 
-	router.Run(":8888")
+	router.Run(fmt.Sprintf(":%d", port))
 }
